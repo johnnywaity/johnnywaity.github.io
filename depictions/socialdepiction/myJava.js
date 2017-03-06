56474ce85d3aa2d14bbd020b0f5ad0458d0f1802
johnnywaity <johnnywaity@gmail.com>
2017-03-05 20:18:27 -0600

    social

---
+++ depictions/socialdepiction/myJava.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Johnny Waity on 3/5/2017.
+ */
+function paypal() {
+    window.location.href='https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=BV9L9ASVSFCSL&lc=US&item_name=repo&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted'
+}
